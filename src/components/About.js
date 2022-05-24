4ba6a02d0825e60579e466366e8e2c97ed034df1
chriskavanagh <ckava3@gmail.com>
2022-05-24 11:00:07 +0000

    About change

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>About Section</h1>
       <h3>This is the About Section of this app.</h3>
       <h4>Thank you for stopping by!</h4>
     </div>
