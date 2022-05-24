218ecd7cca36ca0af4ada5ddbc6d22a704ee4e69
chriskavanagh <ckava3@gmail.com>
2022-05-24 11:10:48 +0000

    Home comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import Home from "./components/Home";
 import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Hello Chris Kavanagh</h1>
+        <h1>Hello Chris Kavanagh</h1>
         <h2>Web Dev</h2>
         <h3>Roanoke, VA 24018</h3>
-        <About/>
+        <Home />
+        <About />
       </header>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Section</h1>
+      <h3>Welcome to our website!</h3>
+      <h4>Created by Me! Chris Kavanagh</h4>
+    </div>
+  );
+}
