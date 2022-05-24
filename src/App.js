fa20b05531c35a5e1cb99cc35c29ae09f3cfd4e1
chriskavanagh <ckava3@gmail.com>
2022-05-24 10:57:56 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello Chris Kavanagh</h1>
+        <h2>Web Dev</h2>
+        <h3>Roanoke, VA 24018</h3>
+        <About/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <h3>This is the About Section of this app.</h3>
+      <h4>Thank you for stopping by!</h4>
+    </div>
+  );
+}
